562c572171b5d451e8ab7503d35a0c2b300404dc
unknown <yazid.ben-madani@epita.fr>
2024-04-08 03:40:47 +0200

    finish flake8

--- Churn_predict/__init__.py
+++ Churn_predict/__init__.py
@@ -1,11 +1,8 @@
 selected_features = [
-   'CustomerID', 
-    'PreferredLoginDevice', 
-    'CityTier', 
-    'PreferredPaymentMode', 
+    'CustomerID',
+    'PreferredLoginDevice',
+    'CityTier',
+    'PreferredPaymentMode',
     'Gender',
-    'NumberOfDeviceRegistered']
-
-categorical_columns = ['PreferredLoginDevice', 'PreferredPaymentMode', 'Gender']
-
-continuous_columns = ['CustomerID', 'CityTier', 'NumberOfDeviceRegistered']
+    'NumberOfDeviceRegistered',
+]
